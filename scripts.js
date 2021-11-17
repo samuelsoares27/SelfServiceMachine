65e2efdeb79e00e40198fa16bb05000434c70b6b
Samuel Soares Guimarães <34168199+samuelsoares27@users.noreply.github.com>
2021-11-17 10:18:18 -0300

    Finished code

---
+++ scripts.js
@@ -0,0 +1,88 @@
+var products = [
+    {
+        "photo": "img/big-mac.png",
+        "name": "Big Mac",
+        "price": 5.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/mc-chicken.png",
+        "name": "Mc Chicken",
+        "price": 4.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/double-cb.png",
+        "name": "Double Cheese Burger",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/fries.png",
+        "name": "Batata frita",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nuggets.png",
+        "name": "Mc Nuggets",
+        "price": 3.49,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/salad.png",
+        "name": "Salada",
+        "price": 2.79,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/cola.png",
+        "name": "Coca Cola",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/lipton.png",
+        "name": "Ice Tea",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/water.png",
+        "name": "Água",
+        "price": 1.49,
+        "active": false,
+        "quantity": 1
+    }
+];
+ 
+const SelfServiceMachine = {
+    data() {
+        return {
+            products: window.products
+        }
+    },
+    methods: {
+        Total: function() {
+            var total = 0;
+
+            this.products.forEach(element => {
+                if(element.active){
+                    total += element.price * element.quantity; 
+                }
+            });
+
+            return total.toFixed(2);
+        }
+    }
+};
+
+Vue.createApp(SelfServiceMachine).mount('#app');
